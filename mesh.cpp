e074808e1977e29d97e396ec453b26bfff80e989
brian.shourd <brian.shourd@eb85dc16-8f5f-11dd-9a11-5fe6a12f7d96>
2008-10-09 12:16:36 +0000

    Added a few more functions to the Camera class, it's probably finished until SDLloader is more fleshed out.
    Added the ability to load generic .obj files into the mesh class. As of now, it ignores a good deal of the information contained in the .obj file, but it does load a good mesh that can be displayed and has lighting set up.
    
    Added a (really poorly coded) example of how the camera functions can work and loading a mesh. It's not very clear, I'll fix it when I get the chance.

--- mesh.cpp
+++ mesh.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 #include "mesh.h"
 
@@ -38,14 +39,96 @@ Mesh::Mesh(std::string filename)
     instream.close();
 }
 
+void Mesh::loadObj(std::string filename)
+{
+    std::fstream instream;
+    instream.open(filename.c_str());
+    //printf("Mesh::Loading mesh from %s\n", filename.c_str());
+    if (!instream.is_open())
+    {
+        //printf("Mesh::Error openin file");
+        return;
+    }
+    while (!instream.eof())
+    {
+        std::string currentLine;
+        getline(instream, currentLine);
+        //printf("Mesh::Currentline is %s\n", currentLine.c_str());
+        if (currentLine != "")  //Make sure that there is a first character to avoid errors
+        {
+            int temp;   //Used for throwing away dumps in sscanf
+            if (sscanf(currentLine.c_str(), "v %f%*s", &temp) == 1) {
+                //printf("Mesh::Pushing back a vector\n");
+                vertices.push_back(Point3(currentLine));
+            } else if (sscanf(currentLine.c_str(), "f %f%*s", &temp) == 1) {
+                //printf("Mesh::Pushing back a face\n");
+                std::vector<Point3> facePoints;
+                std::string facePiece = "";
+                std::stringstream curLineStream(currentLine);
+                getline(curLineStream, facePiece, ' '); //Get the piece that is just 'f'
+                while(!curLineStream.eof())
+                {
+                    getline(curLineStream, facePiece, ' ');
+                    //printf("Mesh::facePiece is %s, ", facePiece.c_str());
+                    //For now, just get the vertex, ignore the other stuff
+                    int vertIndex;
+                    if (sscanf(facePiece.c_str(), "%i%*s", &vertIndex) == 1) {    //If it finds it
+                        vertIndex -= 1;     //For whatever reason, in .obj files, each list starts with index 1
+                        //printf("vertIndex is %i\n", vertIndex);
+                        if (vertIndex >= 0 && vertIndex < vertices.size()) {      //If the index is in a valid range
+                            facePoints.push_back(vertices[vertIndex]);
+                        } else {
+                            printf("Error: vertIndex in MeshObjLoader was out of range\n");
+                        }
+                    } else {
+                        printf("Error: MeshObjLoader: %s is not the proper format for a face piece\n", facePiece.c_str());
+                    }
+                }
+                addFaceAndReverse(facePoints);
+            } else if (sscanf(currentLine.c_str(), "vt %f%*s", &temp) == 1) {
+                //This is where I would do something with the texture guys
+            } else if (sscanf(currentLine.c_str(), "v %f%*s", &temp) == 1) {
+                //This is where I would do something with the normal guys
+            } else {
+                printf("Got something in the Obj file that wasn't what I expected: %s\n", currentLine.c_str());
+            }
+        }
+    }
+
+    /* For printing out a mesh after loading * /
+    printf("Loaded a mesh, vertices are:\n");
+    for (int i = 0; i < vertices.size(); i++)
+    {
+        printf("   %i: <%f, %f, %f>\n", i, vertices[i].x, vertices[i].y, vertices[i].z);
+    }
+    /**/
+
+    instream.close();
+}
+
 void Mesh::addFace(std::vector<Point3>& points)
 {
     Face newFace;
+    //printf("Adding a face: vertices to follow:\n");
     for (int i = 0; i < points.size(); i++)
     {
+        //printf("    <%f, %f, %f>\n", points[i].x, points[i].y, points[i].z);
         newFace.addPoint(points[i]);
     }
     faces.push_back(newFace);
+}
+
+//Adds two faces, facing in both directions, so that one can be sure that neither side is see through
+void Mesh::addFaceAndReverse(std::vector<Point3>& points)
+{
+    addFace(points);
+    std::vector<Point3> newPoints;
+    newPoints.resize(points.size());
+    for (int i = 0; i < points.size(); i++)
+    {
+        newPoints[i] = points[points.size() - i - 1];
+    }
+    addFace(newPoints);
 }
 
 void Mesh::drawMesh()
