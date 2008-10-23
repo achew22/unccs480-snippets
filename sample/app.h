fc0a5231ce639b38b23c86e79d3a6618712b206a
achew22 <achew22@eb85dc16-8f5f-11dd-9a11-5fe6a12f7d96>
2008-10-23 03:44:22 +0000

    Replacing sample app (may not work)

---
+++ sample/app.h
@@ -0,0 +1,32 @@
+#ifndef APP_H_INCLUDED
+#define APP_H_INCLUDED
+
+#include <SDL/SDL.h>
+
+#include "SDLLoader.h"
+#include "mesh.h"
+
+#include "carCharacter.h"
+#include "sampleServer.h"
+
+class App : public SDLLoader {
+private:
+    CarCharacter * character;
+    int textureId;
+
+    SampleServer * server;
+
+public:
+
+    static App * pinstance;
+    static App * getInstance();
+
+    static bool key(SDL_Event event);
+
+    App();
+
+    void display();
+    void idle();
+};
+
+#endif // APP_H_INCLUDED
---
+++ sample/sampleServer.h
@@ -0,0 +1,16 @@
+#ifndef SAMPLESERVER_H_INCLUDED
+#define SAMPLESERVER_H_INCLUDED
+
+#include <string>
+
+#include <SDL/SDL_net.h>
+
+#include "serverConnection.h"
+
+class SampleServer : public ServerConnection {
+public:
+    SampleServer(int setPort);
+    void handleData(int conectionId, std::string data);
+};
+
+#endif // SAMPLESERVER_H_INCLUDED
